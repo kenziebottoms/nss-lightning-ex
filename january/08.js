4bf4b50313ba25fd091ba352a25caa7df732bd81
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-01-08 09:33:50 -0600

    j8

---
+++ january/08.js
@@ -0,0 +1,53 @@
+let customers = {
+    active: {
+        a87b6345: {
+            name: "Gary Harrison",
+            age: 23,
+            member_level: "gold"
+        },
+        b345hy66: {
+            name: "Harry Garrison",
+            age: 34,
+            member_level: "silver"
+        },
+        nh56yht8: {
+            name: "Mary Terryson",
+            age: 78,
+            member_level: "platinum"
+        },
+        j677yd456: {
+            name: "Barry Sonnyman",
+            age: 44,
+            member_level: "gold"
+        }
+    },
+    inactive: {
+        ur56tfgt: {
+            name: "Barb Justice",
+            age: 56,
+            member_level: "none"
+        },
+        d56gt6: {
+            name: "Jane L'eggo",
+            age: 11,
+            member_level: "none"
+        }
+    }
+}
+
+// ref: https://stackoverflow.com/questions/8312459/iterate-through-object-properties
+for (let property in customers.active) {
+    if (customers.active.hasOwnProperty(property)) {
+        let object = customers.active[property];
+        object.id = property;
+    }
+}
+for (let property in customers.inactive) {
+    if (customers.inactive.hasOwnProperty(property)) {
+        let object = customers.inactive[property];
+        object.id = property;
+    }
+}
+
+document.getElementById("result").innerHTML = JSON.stringify(customers);
+console.log(customers);
--- january/README.md
+++ january/README.md
@@ -3,7 +3,7 @@
 |S|M|T|W|T|F|S|
 |-|-|-|-|-|-|-|
 ||1|[2](#j2)|[3](#j3)|4|5|6|
-|7|8|9|10|11|12|13|
+|7|[8](#j8)|9|10|11|12|13|
 |14|15|16|17|18|19|20|
 |21|22|23|24|25|26|27|
 |28|29|30|31||||
@@ -52,4 +52,49 @@ Choose a loop method of your choice to get `[3, 5, 7, 9, 11, 13]` from the follo
 
 ```Javascript
 let nums = [1, 2, 3, 4, "5", 6, "7"];
+```
+
+<a id="j8"></a>
+
+#### January 8, 2018
+
+Loop through this object and add each member object's key as an ID. Log the results.
+
+```js
+let customers = {
+    active: {
+        a87b6345: {
+            name: "Gary Harrison",
+            age: 23,
+            member_level: "gold"
+        },
+        b345hy66: {
+            name: "Harry Garrison",
+            age: 34,
+            member_level: "silver"
+        },
+        nh56yht8: {
+            name: "Mary Terryson",
+            age: 78,
+            member_level: "platinum"
+        },
+        j677yd456: {
+            name: "Barry Sonnyman",
+            age: 44,
+            member_level: "gold"
+        }
+    },
+    inactive: {
+        ur56tfgt: {
+            name: "Barb Justice",
+            age: 56,
+            member_level: "none"
+        },
+        d56gt6: {
+            name: "Jane L'eggo",
+            age: 11,
+            member_level: "none"
+        }
+    }
+}
 ```
